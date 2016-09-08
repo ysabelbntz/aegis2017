47915c753d4ca7e319d70fc68a2ae1c1963d173f
imrobberting <robert.cuartero@obf.ateneo.edu>
2016-09-09 00:18:25 +0800

    Removed bug that does not allow non-admin to see calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_admin!
+  
 
   # GET /events
   # GET /events.json
