fdde206636da3942534b751fcf1514f8f9726594
cdyap <cdyap@outlook.com>
2016-09-09 15:26:15 +0800

    Fixed Admins

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,6 +2,20 @@ class AdminsController < ApplicationController
 	before_filter :authenticate_admin!
 
 	def index
+		@soh_students = Student.where(school: "SOH").count
+		@som_students = Student.where(school: "SOM").count
+		@sose_students = Student.where(school: "SOSE").count
+		@soss_students = Student.where(school: "SOSS").count
+		@students = @soh_students + @som_students + @sose_students + @soss_students
+
+		@soh_accounts = Account.where(school: "SOH").count
+		@som_accounts = Account.where(school: "SOM").count
+		@sose_accounts = Account.where(school: "SOSE").count
+		@soss_accounts = Account.where(school: "SOSS").count
+		@accounts = @soh_accounts + @som_accounts + @sose_accounts + @soss_accounts
+	end
+
+	def students
 		@students = Student.all.order('id ASC').paginate(:page => params[:page], :per_page => 30)
 	end
 
