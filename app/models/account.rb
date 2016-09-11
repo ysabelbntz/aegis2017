6573e5ec9fe152bec0638b041e81ba6a8cb83c92
cdyap <cdyap@outlook.com>
2016-09-11 09:38:11 +0800

    Model update

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,7 +4,7 @@ class Account < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:student_id]
   
-  validates_uniqueness_of :email, :id
+  validates_uniqueness_of :email, :student_id
   
   def self.find_for_database_authentication(warden_conditions)
       conditions = warden_conditions.dup
