f46a3a2f72b841420888d414cff1048c974bdd30
cdyap <cdyap@outlook.com>
2016-09-23 22:47:14 +0800

    Mail hot fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'afternoon-mesa-52954.herokuapp.com'
+  host = 'aegis2017.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -88,7 +88,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'aegis2017.com',
     :enable_starttls_auto => true
   }
 end
