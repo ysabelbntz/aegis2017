b4861b6f3c6cf7f3697e8eab9c0afd3a503ab0e5
cdyap <cdyap@outlook.com>
2016-09-24 17:57:06 +0800

    Email fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'aegis2017.com',
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
