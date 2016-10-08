aa2f80a7c0f82cdf8f7be41afb0839d2c6d9fd0d
cdyap <cdyap@outlook.com>
2016-10-08 22:12:11 +0800

    DB migrate

--- db/migrate/20161001032342_remove_created_at_from_groupslots.rb
+++ db/migrate/20161001032342_remove_created_at_from_groupslots.rb
@@ -3,6 +3,6 @@ class RemoveCreatedAtFromGroupslots < ActiveRecord::Migration
   	remove_column :groupslots, :created_at
   	remove_column :groupslots, :updated_at
   	remove_column :timeslots, :group_name
-  	drop_table :groupshots
+  	# drop_table :groupshots
   end
 end
